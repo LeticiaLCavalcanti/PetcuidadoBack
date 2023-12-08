dd797e5d974cc96491a612e8cb11a81d89418c2f
LeticiaLCavalcanti <wmli515@gmail.com>
2023-12-08 02:59:44 -0300

    take off the swagger for now

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import express from "express";
 import cors from "cors";
 import connectDatabase from "./src/database/database.js";
 import router from "./src/routes/index.js";
-import swaggerRouter from "./src/routes/swagger.route.cjs";
 
 const app = express();
 
@@ -11,6 +10,5 @@ connectDatabase();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use("/doc", swaggerRouter);
 
 export default app;
